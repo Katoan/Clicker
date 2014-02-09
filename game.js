edf385851a20091bbb004c66d3bf19f749d3d092
Pekka Rantala <pjrant@utu.fi>
2014-02-09 23:34:34 +0200

    Added functionality
    - Added more functionality to the game.
    - Added the new description stream, that gets updated every time a new
    item is bought for the first time.
    - Rationalized the way the items are populated in the shop
    - Rationalized the way the item attributes are updated when new items
    are bought (no longer loading the whole shoplist again)
    - Commented the code to make it faster to go through it
    - Added item description property to the items with some lorem ipsum

--- game.js
+++ game.js
@@ -1,23 +1,39 @@
+/*
+start the game when the document is loaded
+*/
 $(document).ready(function() {
 	startGame();
 });
 
+/*
+the game,
+
+game data is a list of items that are in the game
+the game data (JSON) items have to have name (string), cps (number), price (number) and description (string) with those exact names
+later more item properties will be added
+*/
+
 function game(gameData) {
+	// initialize all the variables of the game
 	clicks = 0;
 	onClick = 1;
 	clicksPerSecond = 0;
 	boughtItems = {};
 	var shopPopulated = 0;
 	var shopList = gameData;
+	// populate the bought items list with 0 of all in the game data
 	populateBoughtItems(shopList);
+	// populate the shop in the UI
 	populateShop(shopList);
+	// start the autoclicker interval
 	setInterval(function() {
 		addClicks(clicksPerSecond/10);
 	}, 100);
+	// give the clicker functionality
 	$("#clickMe").click(function() {
 		addClicks(onClick);
 	});
-	
+
 	function getClicksPerSecond() {
 		return clicksPerSecond;
 	}
@@ -50,9 +66,20 @@ function game(gameData) {
 				$('<div id="owned'+item.name+'"></div>').appendTo("#owned");
 			}
 			$("#kauppa"+item.name).text('');
+			// generate item property divs inside the general div to populate the shop list
+			$('<div id="kauppa'+item.name+'Name"></div>').appendTo("#kauppa"+item.name);
+			$('<div id="kauppa'+item.name+'Cps"></div>').appendTo("#kauppa"+item.name);
+			$('<div id="kauppa'+item.name+'Price"></div>').appendTo("#kauppa"+item.name);
+			$("#kauppa"+item.name+"Name").text("Name : "+item.name);
+			$("#kauppa"+item.name+"Cps").text("Clicks per Second : "+item.cps);
+			$("#kauppa"+item.name+"Price").text("Value : "+item.price);
+			
+			/*
 			$.each(item, function(key, value) {
 				$("#kauppa"+item.name).append(key+" : "+value+"<br />");
 			});
+			*/
+			// give the shop items update functionality on click
 			$("#kauppa"+item.name).click(function() {
 				buyItem(item);
 				});
@@ -60,32 +87,43 @@ function game(gameData) {
 		shopPopulated = 1;
 	}
 	
+	// initialize the item inventory
 	function populateBoughtItems(shopItems) {
 		$.each(shopItems, function(i, item) {
 			boughtItems[item.name] = 0;
 		});
 	}
 
+	// buy the item, if enough money when you click
 	function buyItem(item) {
 		if (clicks >= item.price) {
-		updateBoughtItems(item.name);
+		updateBoughtItems(item);
 		addClicks(-item.price);
 		addClicksPerSecond(item.cps);
+		// I'll use a standard multiplier for this for now
 		item.price = Math.floor(item.price*1.3);
-		populateShop(shopList);
+		//update the new price to the item
+		$("#kauppa"+item.name+"Price").text("Value : "+item.price);
+		//populateShop(shopList);
 		}
 	}
 	
-	function updateBoughtItems(itemName) {
-		if (boughtItems[itemName] != 'undefined') {
-			boughtItems[itemName] += 1;
-		} else {
-			boughtItems[itemName] = 1;
+	// update bought item list, when you buy an item
+	function updateBoughtItems(item) {
+		if (boughtItems[item.name] != 'undefined') {
+			boughtItems[item.name] += 1;
 		}
-		$("#owned"+itemName).text(""+itemName+": "+boughtItems[itemName]+"");
+		$("#owned"+item.name).text(""+item.name+": "+boughtItems[item.name]+"");
+		// if the item was bought for the first time, add the description text to the top of the description stream
+		if (boughtItems[item.name] == 1) {
+			$("#descriptionStream").prepend('<div class="description">'+item.description+'</div>');
+		}
 	}
 }	
 
+/*
+Load the game data and start the game passing the game data as an argument
+*/
 function startGame() {
 	$.getJSON("shoppables.json", function(data) {
 		var items = [];
